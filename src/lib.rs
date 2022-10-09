10b76809a3284e02495ef531ee043651dfd6c6d5
Abb4 <abby.abb@web.de>
2022-10-09 20:17:57 +0200

    added basic documentation and usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,62 @@
+//! # bevy-mouse-position-component
+//! Wrapps the implementation of [Custom Camera Projection](https://bevy-cheatbook.github.io/cookbook/custom-projection.html#custom-camera-projection) into a `MousePosition2d` component, which is updated every frame to contain the cursor coordinates as world coordinates.
+//!
+//! # Usage
+//!
+//! Add [`MousePositionPlugin`] to your app:
+//!
+//! ```ignore
+//! use bevy::prelude::*;
+//! use bevy_mouse_position_component::{MousePosition2d, MousePositionPlugin};
+//!
+//! fn main() {
+//!     App::new()
+//!         .add_plugins(DefaultPlugins)
+//!         .add_plugin(MousePositionPlugin)
+//!         .run();
+//! }
+//! ```
+//!
+//! Add [`MousePosition2d`] component to your camera(-bundle):
+//!
+//! ```ignore
+//! fn main() {
+//!     App::new()
+//!         // ...
+//!         .add_startup_system(add_camera_with_tracking)
+//!         // ...
+//! }
+//!
+//!
+//! fn add_camera_with_tracking(mut commands: Commands) {
+//!     commands
+//!         .spawn()
+//!         .insert_bundle(Camera2dBundle::default())
+//!         .insert(MousePosition2d::default()); // component added here
+//! }
+//! ```
+//!
+//! Finally add some system to query [`MousePosition2d`] and use `world_pos`:
+//!
+//! ```ignore
+//! fn main() {
+//!     App::new()
+//!         // ...
+//!         .add_system(print_camera_position)
+//!         // ...
+//! }
+//!
+//! fn print_camera_position(query: Query<&MousePosition2d>) {
+//!     let mouse_position = query.single();
+//!
+//!     println!("{}", mouse_position.world_pos);
+//! }
+//! ```
+//!
+//! # Limitations
+//!
+//! [`MousePosition2d`] must be attached to a single `Camera` in the world, otherwise the plugin panics.
+
 use bevy::{
     prelude::{App, Camera, Component, GlobalTransform, Plugin, Query, Res, Vec2, Windows},
     render::camera::RenderTarget,
